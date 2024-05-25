d33faf43340073d2fd00d7921cfc7fd31d6f557c
Tenzin Sim <tenzinsim@gmail.com>
2024-05-25 23:21:23 +0200

    added mui package and theme

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
-
+import { Box } from "@mui/material"
 
 
 const HomePage = () => {
 
     return(
         <div>
+            <Box textAlign={'center'}>
             Hi
+            </Box>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { darkTheme } from './utils/utils';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <ThemeProvider theme={darkTheme}>
+     <CssBaseline />
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
